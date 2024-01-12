065821ef16ff0fb86e78e41e48c0e96ae4606e22
shivanand <shivanandchoudhary22@gmail.com>
2024-01-12 13:02:21 +0530

    Product Cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,19 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../states/app.state';
 import { selectCartProducts } from '../states/cart/cart.selector';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgFor],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
 export class CartComponent implements OnInit {
   constructor(private store: Store<AppState>) {}
   cartItems$ = this.store.select(selectCartProducts);
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 }
