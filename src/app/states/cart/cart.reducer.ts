ece06e5d336da8541507b73d2794603622b6ad9a
shivanand <shivanandchoudhary22@gmail.com>
2024-01-13 10:54:32 +0530

    method 2 to calculate total price

--- src/app/states/cart/cart.reducer.ts
+++ src/app/states/cart/cart.reducer.ts
@@ -17,6 +17,13 @@ export const initialCartState: CartState = {
   total: 0,
 };
 
+export function calculateTotalPrice(products: IProduct[]) {
+  return products.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+}
+
 export const cartReducer = createReducer(
   initialCartState,
   on(addToCart, (state, { product }) => {
@@ -26,8 +33,14 @@ export const cartReducer = createReducer(
         item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
       );
     } else updatedProducts = [...state.products, { ...product, quantity: 1 }];
-    const updatedTotal = +(state.total + product.price).toFixed(2);
-    return { ...state, products: updatedProducts, total: updatedTotal };
+
+    // const updatedTotal = +(state.total + product.price).toFixed(2);
+    // return { ...state, products: updatedProducts, total: updatedTotal };
+    return {
+      ...state,
+      products: updatedProducts,
+      total: calculateTotalPrice(updatedProducts),
+    };
   }),
 
   on(incrementProduct, (state, { productId }) => {
@@ -36,12 +49,17 @@ export const cartReducer = createReducer(
         ? { ...product, quantity: product.quantity + 1 }
         : product
     );
-    const targetProduct = state.products.find(
-      (product) => product.id === productId
-    )!;
-    const updatedTotal = +(state.total + targetProduct.price).toFixed(2);
+    // const targetProduct = state.products.find(
+    //   (product) => product.id === productId
+    // )!;
+    // const updatedTotal = +(state.total + targetProduct.price).toFixed(2);
 
-    return { ...state, products: updatedProducts, total: updatedTotal };
+    // return { ...state, products: updatedProducts, total: updatedTotal };
+
+    return {
+      ...state,
+      products: updatedProducts,
+      total: calculateTotalPrice(updatedProducts)}
   }),
 
   on(decrementProduct, (state, { productId }) => {
@@ -53,26 +71,37 @@ export const cartReducer = createReducer(
     const targetProduct = state.products.find(
       (product) => product.id === productId
     )!;
-    const updatedTotal = +(state.total - targetProduct.price).toFixed(2);
+    // const updatedTotal = +(state.total - targetProduct.price).toFixed(2);
     if (targetProduct.quantity == 1) {
       console.log('Target Product is now null');
       updatedProducts = state.products.filter((item) => productId != item.id);
-
     }
-    return { ...state, products: updatedProducts, total: updatedTotal };
+    // return { ...state, products: updatedProducts, total: updatedTotal };
+
+    return {
+      ...state,
+      products: updatedProducts,
+      total: calculateTotalPrice(updatedProducts),
+    };
   }),
 
   on(removeProduct, (state, { productId }) => {
     const updatedProducts = state.products.filter(
       (item) => productId != item.id
     );
-    const targetProduct = state.products.find(
-      (product) => product.id === productId
-    )!;
-    const updatedTotal = +(
-      state.total -
-      targetProduct.quantity * targetProduct.price
-    ).toFixed(2);
-    return { ...state, products: updatedProducts, total: updatedTotal };
+    // const targetProduct = state.products.find(
+    //   (product) => product.id === productId
+    // )!;
+    // const updatedTotal = +(
+    //   state.total -
+    //   targetProduct.quantity * targetProduct.price
+    // ).toFixed(2);
+    // return { ...state, products: updatedProducts, total: updatedTotal };
+
+    return {
+      ...state,
+      products: updatedProducts,
+      total: calculateTotalPrice(updatedProducts),
+    };
   })
 );
